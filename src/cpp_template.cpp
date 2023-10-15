9228ff4181874e0b8a71e04ad03f0e08b710f8cc
Alpaca-zip <zip.lottestr@gmail.com>
2023-10-15 14:19:47 +0900

    modifying the package configuration

--- src/cpp_template.cpp
+++ src/cpp_template.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2023  template
+ * Copyright (C) 2023  Alpaca-zip
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
