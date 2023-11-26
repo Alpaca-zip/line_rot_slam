dbfb5d289d85318a3174963df8ec3780351916e8
Alpaca-zip <zip.lottestr@gmail.com>
2023-11-27 03:45:34 +0900

    add message_filters and update optimization algorithm

--- include/map_optimization_nodelet/map_optimization_nodelet.h
+++ include/map_optimization_nodelet/map_optimization_nodelet.h
@@ -24,15 +24,21 @@
 #include <nav_msgs/Odometry.h>
 #include <geometry_msgs/Point.h>
 #include <visualization_msgs/MarkerArray.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/exact_time.h>
 #include <actionlib/server/simple_action_server.h>
 #include <line_rot_slam/OptimizationAction.h>
 #include <pcl_ros/point_cloud.h>
 #include <pcl/common/transforms.h>
+#include <pcl/kdtree/kdtree_flann.h>
 #include <pcl/filters/voxel_grid.h>
+#include <opencv2/opencv.hpp>
 #include <tf2_eigen/tf2_eigen.h>
 #include <tf2_ros/transform_listener.h>
 #include <tf2_ros/transform_broadcaster.h>
 
+typedef message_filters::sync_policies::ExactTime<sensor_msgs::PointCloud2, sensor_msgs::PointCloud2> SyncPolicy;
 typedef actionlib::SimpleActionServer<line_rot_slam::OptimizationAction> Server;
 
 namespace OPTIMIZATION
@@ -42,23 +48,28 @@ class MapOptimizationNodelet : public nodelet::Nodelet
 private:
   ros::NodeHandle nh_;
   ros::NodeHandle pnh_;
-  ros::Subscriber pointcloud_sub_;
   ros::Publisher marker_pub_;
   ros::Publisher map_pub_;
   ros::Timer timer_;
   std::string points_in_;
+  std::string feature_points_in_;
   std::string odom_frame_;
   std::string map_frame_;
-  std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr> cloud_vector_;
-  std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr> transformed_cloud_vector_;
+  std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr> map_cloud_vector_;
+  std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr> feature_cloud_vector_;
   std::vector<geometry_msgs::Point> old_marker_points_;
   std::vector<Eigen::Affine3d> transform_vector_;
   Eigen::Affine3d prev_transform_;
   Eigen::Affine3d prev_optimization_transform_;
   pcl::PointCloud<pcl::PointXYZI>::Ptr map_cloud_;
+  pcl::PointCloud<pcl::PointXYZI>::Ptr optimized_cloud_container_;
+  pcl::KdTreeFLANN<pcl::PointXYZI>::Ptr kdtree_feature_cloud_;
+  message_filters::Subscriber<sensor_msgs::PointCloud2> cloud_sub_;
+  message_filters::Subscriber<sensor_msgs::PointCloud2> feature_cloud_sub_;
   boost::shared_ptr<tf2_ros::Buffer> tf_buffer_;
   boost::shared_ptr<tf2_ros::TransformListener> tf_listener_;
   boost::shared_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
+  boost::shared_ptr<message_filters::Synchronizer<SyncPolicy>> sync_;
   boost::shared_ptr<Server> server_;
   bool optimization_flag_;
   int marker_id_;
@@ -69,14 +80,30 @@ private:
 public:
   void onInit() override;
   void broadcastMapFrame(const ros::TimerEvent& event);
-  void pointcloudCallback(const sensor_msgs::PointCloud2ConstPtr& cloud_msg);
+  void pointcloudCallback(const sensor_msgs::PointCloud2ConstPtr& cloud_msg,
+                          const sensor_msgs::PointCloud2ConstPtr& feature_cloud_msg);
   void execute(const line_rot_slam::OptimizationGoalConstPtr& goal);
-  void transformCloud();
+  void moveOptimization();
+  void turnOptimization();
+  void moveAndAppendCloud(const pcl::PointCloud<pcl::PointXYZI>::Ptr& input_cloud,
+                          const Eigen::Vector3d& transform_offset, pcl::PointCloud<pcl::PointXYZI>::Ptr& output_cloud);
+  void turnAndAppendCloud(const pcl::PointCloud<pcl::PointXYZI>::Ptr& input_cloud,
+                          const Eigen::Matrix3d& rotation_matrix, pcl::PointCloud<pcl::PointXYZI>::Ptr& output_cloud);
+  void moveOptimizeAndAppendCloud(const pcl::PointCloud<pcl::PointXYZI>::Ptr& input_cloud,
+                                  const Eigen::Vector3d& direction, const double& error);
+  void turnOptimizeAndAppendCloud(const pcl::PointCloud<pcl::PointXYZI>::Ptr& input_cloud, const double& error);
+  void moveOptimizationCore(const pcl::PointCloud<pcl::PointXYZI>::Ptr& map_cloud,
+                            const pcl::PointCloud<pcl::PointXYZI>::Ptr& current_cloud,
+                            const Eigen::Vector3d& optimize_direction, std::vector<double>& direction_error_vector);
+  void turnOptimizationCore(const pcl::PointCloud<pcl::PointXYZI>::Ptr& map_cloud,
+                            const pcl::PointCloud<pcl::PointXYZI>::Ptr& current_cloud,
+                            std::vector<double>& direction_error_vector);
   void publishMarkerArray();
   void publishMapCloud();
   bool getTransform(const std::string& target_frame, const std::string& source_frame, const ros::Time& time,
                     Eigen::Affine3d& transform);
   bool shouldPushBackCloud(const Eigen::Affine3d& current_transform, Eigen::Affine3d& incremental_transform);
+  double calculateMedianError(std::vector<double>& direction_error_vector);
 };
 
 }  // namespace OPTIMIZATION
--- src/map_optimization_nodelet.cpp
+++ src/map_optimization_nodelet.cpp
@@ -22,13 +22,18 @@ void MapOptimizationNodelet::onInit()
 {
   pnh_ = getPrivateNodeHandle();
   pnh_.param<std::string>("input_points_topic", points_in_, "points_in");
+  pnh_.param<std::string>("input_feature_points_topic", feature_points_in_, "feature_points_in");
   pnh_.param<std::string>("odom_frame", odom_frame_, "odom");
   pnh_.param<std::string>("map_frame", map_frame_, "map");
   pnh_.param<double>("translation_threshold", translation_threshold_, 0.1);
   pnh_.param<double>("rotation_threshold", rotation_threshold_, 0.1);
   pnh_.param<double>("voxel_grid_leaf_size", leaf_size_, 0.1);
 
-  pointcloud_sub_ = nh_.subscribe(points_in_, 1, &MapOptimizationNodelet::pointcloudCallback, this);
+  cloud_sub_.subscribe(nh_, points_in_, 1);
+  feature_cloud_sub_.subscribe(nh_, feature_points_in_, 1);
+  sync_ = boost::make_shared<message_filters::Synchronizer<SyncPolicy>>(1);
+  sync_->connectInput(cloud_sub_, feature_cloud_sub_);
+  sync_->registerCallback(boost::bind(&MapOptimizationNodelet::pointcloudCallback, this, _1, _2));
   timer_ = nh_.createTimer(ros::Duration(0.1), &MapOptimizationNodelet::broadcastMapFrame, this);
   marker_pub_ = nh_.advertise<visualization_msgs::MarkerArray>("optimization_marker", 1);
   map_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("map", 1);
@@ -38,6 +43,8 @@ void MapOptimizationNodelet::onInit()
   marker_id_ = 0;
 
   map_cloud_.reset(new pcl::PointCloud<pcl::PointXYZI>);
+  optimized_cloud_container_.reset(new pcl::PointCloud<pcl::PointXYZI>);
+  kdtree_feature_cloud_.reset(new pcl::KdTreeFLANN<pcl::PointXYZI>);
   tf_buffer_.reset(new tf2_ros::Buffer(ros::Duration(2.0), true));
   tf_listener_.reset(new tf2_ros::TransformListener(*tf_buffer_));
   tf_broadcaster_.reset(new tf2_ros::TransformBroadcaster);
@@ -66,7 +73,8 @@ void MapOptimizationNodelet::broadcastMapFrame(const ros::TimerEvent& event)
   tf_broadcaster_->sendTransform(transform_stamped);
 }
 
-void MapOptimizationNodelet::pointcloudCallback(const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
+void MapOptimizationNodelet::pointcloudCallback(const sensor_msgs::PointCloud2ConstPtr& cloud_msg,
+                                                const sensor_msgs::PointCloud2ConstPtr& feature_cloud_msg)
 {
   if (!optimization_flag_)
   {
@@ -77,10 +85,12 @@ void MapOptimizationNodelet::pointcloudCallback(const sensor_msgs::PointCloud2Co
     {
       if (shouldPushBackCloud(current_transform, incremental_transform))
       {
-        pcl::PointCloud<pcl::PointXYZI>::Ptr pcl_cloud(new pcl::PointCloud<pcl::PointXYZI>);
-
-        pcl::fromROSMsg(*cloud_msg, *pcl_cloud);
-        cloud_vector_.push_back(pcl_cloud);
+        pcl::PointCloud<pcl::PointXYZI>::Ptr map_cloud(new pcl::PointCloud<pcl::PointXYZI>);
+        pcl::PointCloud<pcl::PointXYZI>::Ptr feature_cloud(new pcl::PointCloud<pcl::PointXYZI>);
+        pcl::fromROSMsg(*cloud_msg, *map_cloud);
+        pcl::fromROSMsg(*feature_cloud_msg, *feature_cloud);
+        map_cloud_vector_.push_back(map_cloud);
+        feature_cloud_vector_.push_back(feature_cloud);
 
         prev_optimization_transform_ = prev_optimization_transform_ * incremental_transform;
         transform_vector_.push_back(prev_optimization_transform_);
@@ -101,20 +111,15 @@ void MapOptimizationNodelet::execute(const line_rot_slam::OptimizationGoalConstP
 
   optimization_flag_ = true;
 
-  transformCloud();
-
   if (goal->state == "MOVE")
   {
-    // moveOptimization();
+    moveOptimization();
     publishMarkerArray();
   }
-
-  /*
-  if (goal->state == "TURN")
+  else if (goal->state == "TURN")
   {
     turnOptimization();
   }
-  */
 
   publishMapCloud();
 
@@ -123,21 +128,306 @@ void MapOptimizationNodelet::execute(const line_rot_slam::OptimizationGoalConstP
 
   prev_optimization_transform_ = transform_vector_.back();
 
-  cloud_vector_.clear();
-  transformed_cloud_vector_.clear();
+  map_cloud_vector_.clear();
+  feature_cloud_vector_.clear();
   transform_vector_.clear();
 
   optimization_flag_ = false;
 }
 
-void MapOptimizationNodelet::transformCloud()
+void MapOptimizationNodelet::moveOptimization()
 {
-  for (int i = 0; i < cloud_vector_.size(); i++)
+  Eigen::Vector3d direction =
+      (transform_vector_.back().translation() - transform_vector_.front().translation()).normalized();
+  pcl::PointCloud<pcl::PointXYZI>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZI>);
+
+  moveAndAppendCloud(feature_cloud_vector_[0], transform_vector_[0].translation(), cloud);
+
+  if (optimized_cloud_container_->empty())
   {
-    pcl::PointCloud<pcl::PointXYZI>::Ptr cloud = cloud_vector_[i];
+    optimized_cloud_container_ = cloud;
+  }
+
+  for (int i = 1; i < feature_cloud_vector_.size(); i++)
+  {
     pcl::PointCloud<pcl::PointXYZI>::Ptr transformed_cloud(new pcl::PointCloud<pcl::PointXYZI>);
-    pcl::transformPointCloud(*cloud, *transformed_cloud, transform_vector_[i]);
-    transformed_cloud_vector_.push_back(transformed_cloud);
+    std::vector<double> direction_errors;
+
+    moveAndAppendCloud(feature_cloud_vector_[i], transform_vector_[i - 1].translation(), transformed_cloud);
+
+    moveOptimizationCore(optimized_cloud_container_, transformed_cloud, direction, direction_errors);
+
+    double median_error = calculateMedianError(direction_errors);
+
+    moveOptimizeAndAppendCloud(transformed_cloud, direction, median_error);
+
+    transform_vector_[i].translation() = transform_vector_[i - 1].translation() - median_error * direction;
+  }
+}
+
+void MapOptimizationNodelet::turnOptimization()
+{
+  pcl::PointCloud<pcl::PointXYZI>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZI>);
+
+  turnAndAppendCloud(feature_cloud_vector_[0], transform_vector_[0].linear(), cloud);
+
+  if (optimized_cloud_container_->empty())
+  {
+    optimized_cloud_container_ = cloud;
+  }
+
+  for (int i = 1; i < feature_cloud_vector_.size(); i++)
+  {
+    pcl::PointCloud<pcl::PointXYZI>::Ptr transformed_cloud(new pcl::PointCloud<pcl::PointXYZI>);
+    std::vector<double> direction_errors;
+
+    turnAndAppendCloud(feature_cloud_vector_[i], transform_vector_[i - 1].linear(), transformed_cloud);
+
+    turnOptimizationCore(optimized_cloud_container_, transformed_cloud, direction_errors);
+
+    double median_error = calculateMedianError(direction_errors);
+
+    turnOptimizeAndAppendCloud(transformed_cloud, median_error);
+
+    transform_vector_[i].linear() =
+        transform_vector_[i - 1].linear() * Eigen::AngleAxisd(median_error, Eigen::Vector3d::UnitZ());
+  }
+}
+
+void MapOptimizationNodelet::moveAndAppendCloud(const pcl::PointCloud<pcl::PointXYZI>::Ptr& input_cloud,
+                                                const Eigen::Vector3d& transform_offset,
+                                                pcl::PointCloud<pcl::PointXYZI>::Ptr& output_cloud)
+{
+  for (const auto& point : input_cloud->points)
+  {
+    if (!pcl::isFinite(point))
+      continue;
+
+    pcl::PointXYZI transformed_point = point;
+    transformed_point.x += transform_offset(0);
+    transformed_point.y += transform_offset(1);
+    transformed_point.z += transform_offset(2);
+    output_cloud->points.push_back(transformed_point);
+  }
+}
+
+void MapOptimizationNodelet::turnAndAppendCloud(const pcl::PointCloud<pcl::PointXYZI>::Ptr& input_cloud,
+                                                const Eigen::Matrix3d& rotation_matrix,
+                                                pcl::PointCloud<pcl::PointXYZI>::Ptr& output_cloud)
+{
+  for (const auto& point : input_cloud->points)
+  {
+    if (!pcl::isFinite(point))
+      continue;
+
+    Eigen::Vector3d transformed_point = rotation_matrix * Eigen::Vector3d(point.x, point.y, point.z);
+    pcl::PointXYZI new_point;
+    new_point.x = transformed_point(0);
+    new_point.y = transformed_point(1);
+    new_point.z = transformed_point(2);
+    output_cloud->points.push_back(new_point);
+  }
+}
+
+void MapOptimizationNodelet::moveOptimizeAndAppendCloud(const pcl::PointCloud<pcl::PointXYZI>::Ptr& input_cloud,
+                                                        const Eigen::Vector3d& direction, const double& error)
+{
+  for (const auto& point : input_cloud->points)
+  {
+    pcl::PointXYZI optimized_point = point;
+    optimized_point.x -= error * direction(0);
+    optimized_point.y -= error * direction(1);
+    optimized_point.z -= error * direction(2);
+    optimized_cloud_container_->points.push_back(optimized_point);
+  }
+
+  while (optimized_cloud_container_->points.size() > 10000)
+  {
+    optimized_cloud_container_->points.erase(optimized_cloud_container_->points.begin());
+  }
+}
+
+void MapOptimizationNodelet::turnOptimizeAndAppendCloud(const pcl::PointCloud<pcl::PointXYZI>::Ptr& input_cloud,
+                                                        const double& error)
+{
+  Eigen::Matrix3d rotation_matrix;
+  rotation_matrix << cos(error), -sin(error), 0, sin(error), cos(error), 0, 0, 0, 1;
+
+  for (const auto& point : input_cloud->points)
+  {
+    Eigen::Vector3d point_vector(point.x, point.y, point.z);
+    Eigen::Vector3d rotate_vector = rotation_matrix * point_vector;
+    pcl::PointXYZI optimized_point;
+    optimized_point.x = rotate_vector(0);
+    optimized_point.y = rotate_vector(1);
+    optimized_point.z = rotate_vector(2);
+    optimized_cloud_container_->points.push_back(optimized_point);
+  }
+
+  while (optimized_cloud_container_->points.size() > 10000)
+  {
+    optimized_cloud_container_->points.erase(optimized_cloud_container_->points.begin());
+  }
+}
+
+void MapOptimizationNodelet::moveOptimizationCore(const pcl::PointCloud<pcl::PointXYZI>::Ptr& map_cloud,
+                                                  const pcl::PointCloud<pcl::PointXYZI>::Ptr& current_cloud,
+                                                  const Eigen::Vector3d& optimize_direction,
+                                                  std::vector<double>& direction_error_vector)
+{
+  kdtree_feature_cloud_->setInputCloud(map_cloud);
+
+  for (int j = 0; j < current_cloud->size(); j++)
+  {
+    std::vector<int> point_search_index;
+    std::vector<float> point_search_distance;
+
+    pcl::PointXYZI current_point = current_cloud->points[j];
+    kdtree_feature_cloud_->nearestKSearch(current_point, 5, point_search_index, point_search_distance);
+
+    cv::Mat covariance_mat(3, 3, CV_32F, cv::Scalar::all(0));
+    cv::Mat eigen_value(1, 3, CV_32F, cv::Scalar::all(0));
+    cv::Mat eigen_vector(3, 3, CV_32F, cv::Scalar::all(0));
+
+    if (point_search_distance[4] < 1.0)
+    {
+      float center_x = 0, center_y = 0, center_z = 0;
+      for (int k = 0; k < 5; k++)
+      {
+        center_x += map_cloud->points[point_search_index[k]].x;
+        center_y += map_cloud->points[point_search_index[k]].y;
+        center_z += map_cloud->points[point_search_index[k]].z;
+      }
+      center_x /= 5;
+      center_y /= 5;
+      center_z /= 5;
+
+      float a11 = 0, a12 = 0, a13 = 0, a22 = 0, a23 = 0, a33 = 0;
+      for (int k = 0; k < 5; k++)
+      {
+        float ax = map_cloud->points[point_search_index[k]].x - center_x;
+        float ay = map_cloud->points[point_search_index[k]].y - center_y;
+        float az = map_cloud->points[point_search_index[k]].z - center_z;
+
+        a11 += ax * ax;
+        a12 += ax * ay;
+        a13 += ax * az;
+        a22 += ay * ay;
+        a23 += ay * az;
+        a33 += az * az;
+      }
+      a11 /= 5;
+      a12 /= 5;
+      a13 /= 5;
+      a22 /= 5;
+      a23 /= 5;
+      a33 /= 5;
+
+      covariance_mat.at<float>(0, 0) = a11;
+      covariance_mat.at<float>(0, 1) = a12;
+      covariance_mat.at<float>(0, 2) = a13;
+      covariance_mat.at<float>(1, 0) = a12;
+      covariance_mat.at<float>(1, 1) = a22;
+      covariance_mat.at<float>(1, 2) = a23;
+      covariance_mat.at<float>(2, 0) = a13;
+      covariance_mat.at<float>(2, 1) = a23;
+      covariance_mat.at<float>(2, 2) = a33;
+
+      cv::eigen(covariance_mat, eigen_value, eigen_vector);
+
+      if (eigen_value.at<float>(0, 0) > 3 * eigen_value.at<float>(0, 1))
+      {
+        Eigen::Vector3d current_position(current_point.x, current_point.y, current_point.z);
+        Eigen::Vector3d center_position(center_x, center_y, center_z);
+        Eigen::Vector3d position_difference = current_position - center_position;
+        double direction_error = position_difference.dot(optimize_direction.normalized());
+        direction_error_vector.push_back(direction_error);
+      }
+    }
+  }
+}
+
+void MapOptimizationNodelet::turnOptimizationCore(const pcl::PointCloud<pcl::PointXYZI>::Ptr& map_cloud,
+                                                  const pcl::PointCloud<pcl::PointXYZI>::Ptr& current_cloud,
+                                                  std::vector<double>& direction_error_vector)
+{
+  kdtree_feature_cloud_->setInputCloud(map_cloud);
+
+  for (int j = 0; j < current_cloud->size(); j++)
+  {
+    std::vector<int> point_search_index;
+    std::vector<float> point_search_distance;
+
+    pcl::PointXYZI current_point = current_cloud->points[j];
+    kdtree_feature_cloud_->nearestKSearch(current_point, 5, point_search_index, point_search_distance);
+
+    cv::Mat covariance_mat(3, 3, CV_32F, cv::Scalar::all(0));
+    cv::Mat eigen_value(1, 3, CV_32F, cv::Scalar::all(0));
+    cv::Mat eigen_vector(3, 3, CV_32F, cv::Scalar::all(0));
+
+    if (point_search_distance[4] < 1.0)
+    {
+      float center_x = 0, center_y = 0, center_z = 0;
+      for (int k = 0; k < 5; k++)
+      {
+        center_x += map_cloud->points[point_search_index[k]].x;
+        center_y += map_cloud->points[point_search_index[k]].y;
+        center_z += map_cloud->points[point_search_index[k]].z;
+      }
+      center_x /= 5;
+      center_y /= 5;
+      center_z /= 5;
+
+      float a11 = 0, a12 = 0, a13 = 0, a22 = 0, a23 = 0, a33 = 0;
+      for (int k = 0; k < 5; k++)
+      {
+        float ax = map_cloud->points[point_search_index[k]].x - center_x;
+        float ay = map_cloud->points[point_search_index[k]].y - center_y;
+        float az = map_cloud->points[point_search_index[k]].z - center_z;
+
+        a11 += ax * ax;
+        a12 += ax * ay;
+        a13 += ax * az;
+        a22 += ay * ay;
+        a23 += ay * az;
+        a33 += az * az;
+      }
+      a11 /= 5;
+      a12 /= 5;
+      a13 /= 5;
+      a22 /= 5;
+      a23 /= 5;
+      a33 /= 5;
+
+      covariance_mat.at<float>(0, 0) = a11;
+      covariance_mat.at<float>(0, 1) = a12;
+      covariance_mat.at<float>(0, 2) = a13;
+      covariance_mat.at<float>(1, 0) = a12;
+      covariance_mat.at<float>(1, 1) = a22;
+      covariance_mat.at<float>(1, 2) = a23;
+      covariance_mat.at<float>(2, 0) = a13;
+      covariance_mat.at<float>(2, 1) = a23;
+      covariance_mat.at<float>(2, 2) = a33;
+
+      cv::eigen(covariance_mat, eigen_value, eigen_vector);
+
+      if (eigen_value.at<float>(0, 0) > 3 * eigen_value.at<float>(0, 1))
+      {
+        Eigen::Vector3d current_position(current_point.x, current_point.y, current_point.z);
+        Eigen::Vector3d center_position(center_x, center_y, center_z);
+        Eigen::Vector3d cross_product = current_position.cross(center_position);
+        double dot_product = current_position.dot(center_position);
+        double current_norm = current_position.norm();
+        double center_norm = center_position.norm();
+        double cos_angle = dot_product / (current_norm * center_norm);
+        double angle = std::acos(cos_angle);
+
+        if (cross_product.z() < 0)
+          angle = -angle;
+
+        direction_error_vector.push_back(angle);
+      }
+    }
   }
 }
 
@@ -205,9 +495,12 @@ void MapOptimizationNodelet::publishMarkerArray()
 
 void MapOptimizationNodelet::publishMapCloud()
 {
-  for (const pcl::PointCloud<pcl::PointXYZI>::Ptr& cloud : transformed_cloud_vector_)
+  for (size_t i = 0; i < map_cloud_vector_.size(); ++i)
   {
-    *map_cloud_ += *cloud;
+    pcl::PointCloud<pcl::PointXYZI>::Ptr transformed_cloud(new pcl::PointCloud<pcl::PointXYZI>);
+    pcl::transformPointCloud(*map_cloud_vector_[i], *transformed_cloud, transform_vector_[i]);
+
+    *map_cloud_ += *transformed_cloud;
   }
 
   pcl::PointCloud<pcl::PointXYZI>::Ptr downsampled_cloud(new pcl::PointCloud<pcl::PointXYZI>);
@@ -249,6 +542,17 @@ bool MapOptimizationNodelet::shouldPushBackCloud(const Eigen::Affine3d& current_
   double translation_distance = incremental_transform.translation().norm();
 
   return translation_distance >= translation_threshold_ || rotation_angle >= rotation_threshold_;
+}
+
+double MapOptimizationNodelet::calculateMedianError(std::vector<double>& direction_error_vector)
+{
+  if (!direction_error_vector.empty())
+  {
+    std::sort(direction_error_vector.begin(), direction_error_vector.end());
+    return direction_error_vector[direction_error_vector.size() / 2];
+  }
+  else
+    return 0;
 }
 
 }  // namespace OPTIMIZATION
